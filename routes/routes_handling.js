c44ba1e5dbfda2d353eda0a756560a0f0fc9324f
devanshu <devanshukumar45@gmail.com>
2021-06-15 03:46:23 +0530

    added duplication logic

--- routes/routes_handling.js
+++ routes/routes_handling.js
@@ -6,24 +6,39 @@ const Hero = require('../models/hero')
 router.get('/', async (req, res) => {
     try{
         const heros = await Hero.find()
-        res.send(heros)
+        res.json(heros)
     }catch (error){
         res.status(500).json({message: error})
     }
     
 })
 
-router.post('/',  async (req, res) => {    
-    const hero = new Hero({
-        name: req.body.name,
-        superpower: req.body.superpower
+router.post('/',  async (req, res) => {
+    const name = req.body.name
+    var state = false
+    const prevHeros = await Hero.find()
+    prevHeros.forEach((hero) => {
+        if(hero['name'] === name){
+            state = true
+        }
     })
-    try{
-        const newHero = await hero.save()
-        res.status(201).json(newHero)
-    }catch(error){
-        res.status(400).json({message: error.message})
+
+    if(state){
+        res.status(403).json({message: 'Name already exists'})
+        
+    }else{
+        const hero = new Hero({
+            name: req.body.name,
+            superpower: req.body.superpower
+        })
+        try{
+            const newHero = await hero.save()
+            res.status(201).json(newHero)
+        }catch(error){
+            res.status(400).json({message: error.message})
+        }
     }
+
 })
 
 module.exports = router
